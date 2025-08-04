7995c92d4369fbb45f8b155057e158aa4c4d0374
Karolinecf <k.freire@escolar.ifrn.edu.br>
2025-08-04 16:03:09 -0300

    adicionando algumas coisas da rede

--- redes.py
+++ redes.py
@@ -1,1 +1,22 @@
-print("nada")
+import socket;
+import ipaddress;
+import json;
+
+def get_ip_family(host):
+    try:
+        ip = ipaddress.ip_address(host);
+        if isinstance(ip, ipaddress.IPv4Address):
+            return socket.AF_INET;
+        elif isinstance(ip, ipaddress.IPv6Address):
+            return socket.AF_INET6;
+    except ValueError:
+        pass
+
+    try:
+        info = socket.getaddrinfo(host, None);
+        if info:
+            return info[0][0];
+    except socket.gaierror:
+        pass
+
+    return None;
