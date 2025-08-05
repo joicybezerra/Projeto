b9e75ed28e33c6043ee56df5a177f09fd41bf9bd
Karolinecf <k.freire@escolar.ifrn.edu.br>
2025-08-05 13:37:27 -0300

    definição de função de enviar mensagens pela redes

--- redes.py
+++ redes.py
@@ -22,6 +22,17 @@ def criar_socket_comunicacao(protocolo, host, porta):
     sock = socket.socket(family, sock_type)
     #criar socket com a familia IP e o tipo de protocolo escolhido
     sock.setsockopt(socket.socketsetsockopt(socket.SOL_SOCKET, socket.SO_REUSEADOR, 1))
+    #pemite que o servidor seja reiniciado e o erro "porta ocupada" seja evitado
     return sock #retorna ao socket
 
 def enviar_dados(sock, dados, protocolo, oponente_addr=None)
+    #define a função que envia mensagens pela rede
+    #sock = socket usado
+    #dados = dados a serem enviados
+    #protocolo = TCP ou UDP
+    #oponente_addr = Usado no UDP para saber o endereço de destino
+    #Envia dados (serializados em JSON) através do socket.
+    #O oponente_addr é necessário para UDP.
+    payload = json.dumps(dados).encode('utf-8')
+    #playload = pronto para que o socket possa enviar
+    
