7d35eaf12270b2d455266d4e9875227339d36508
joicybezerra <joicybezerra23@gmail.com>
2025-08-05 15:05:51 -0300

    Adicionei o primeiro input do programa.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import socket;
-import threading;
-import time;
+
+modo = input("Deseja hostear (h) ou conectar-se (c) a uma sala? h/c: ")
+# Pergunta ao usuário se deseja hostear ou conectar-se a uma sala
+while modo not in ["h", "c"]:
+#Enquanto o usuário não digitar uma opção válida, continua perguntando
+    print("Opção inválida. Tente novamente.")
+    modo = input("Deseja hostear (h) ou conectar-se (c) a uma sala? h/c: ")
+    
 
