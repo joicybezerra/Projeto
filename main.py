a75ad17fa64aaa17e03e606b82fa792f2ea6d66c
Duardaaraujo <eduarda.ferino@escolar.ifrn.edu.br>
2025-08-06 11:15:57 -0300

    Complementando o while anterior, com outro while, que pergunta e verifica qual o protocolo que será usado, se for tcp/udp o while ira fechar, senão, vai printar uma mensagem informando que não é compativel.

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ import os
 #utilizado para encerrar o programa (os._exit(1))
 
 opcao = input("Deseja hostear (h) ou conectar-se (c) a uma sala? h/c: ").strip().lower()
+
 while opcao not in ["h", "c"]:
     opcao = input("Modo inválido. Use 'h' ou 'c': ").strip().lower()
 if opcao == "h":
    print("Qual protocolo deseja usar para a sala? tpc/udp:")
 else:
    print("Qual o protocolo da sala? tcp/udp:")
+
+while True:
+ protocolo = input().strip().lower()
+ if  protocolo in ["tcp", "udp"]:
+	    break
+print("Protocolo não compatível, peço que por favor use TCP/UDP:")
+
 
 
 
